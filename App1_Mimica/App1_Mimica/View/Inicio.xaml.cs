dde035ed39a169000c9f6d9f9b2498bb992edd90
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-08-29 10:44:40 -0300

    29.08.2020_10:44hs

--- App1_Mimica/App1_Mimica/View/Inicio.xaml.cs
+++ App1_Mimica/App1_Mimica/View/Inicio.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace App1_Mimica
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1_Mimica.View
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
     {
-        public MainPage()
+        public Inicio()
         {
             InitializeComponent();
         }
